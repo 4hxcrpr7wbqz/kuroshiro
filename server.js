b1766ebf3f9461b2fff7cbc4f49f1087ceee6120
Ayaka Mikazuki <ayaka@mail.shn.hk>
2020-08-14 12:08:42 +0800

    Update status messages

--- server.js
+++ server.js
@@ -5,22 +5,40 @@ const KuromojiAnalyzer = require('kuroshiro-analyzer-kuromoji');
 const kuroshiro = new Kuroshiro();
 
 const settings = {
-  to: "hiragana",
-  mode: "okurigana",
-  delimiter_start: "（",
-  delimiter_end: "）"
+	to: 'hiragana',
+	mode: 'okurigana',
+	delimiter_start: '（',
+	delimiter_end: '）'
 };
 
-async function handler(req, res) {
-  req.setEncoding('utf8');
-  req.on('data', async str => {
-    res.end(await kuroshiro.convert(str, settings));
-  });
+const header = { 'Content-Type': 'text/plain; charset=utf-8' };
+
+async function furigana(str) {
+	return await kuroshiro.convert(str, settings);
+}
+
+function handler(req, res) {
+	if (req.method !== 'POST') {
+		res.writeHead(405, header);
+		res.end('Only POST method is supported');
+	} else {
+		const body = [];
+		req.on('data', chunk => body.push(chunk));
+		req.on('end', () => {
+			furigana(Buffer.concat(body).toString()).then(str => {
+				res.writeHead(200, header);
+				res.end(str);
+			}).catch(err => {
+				res.writeHead(500, header);
+				res.end(err.message);
+			});
+		});
+	}
 }
 
 async function run() {
-  await kuroshiro.init(new KuromojiAnalyzer());
-  http.createServer(handler).listen(8000);
+	await kuroshiro.init(new KuromojiAnalyzer());
+	http.createServer(handler).listen(8000);
 }
 
 run();
